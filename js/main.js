23d0afffc0d7175aea6cf1e8c797772aad3be641
ShabbySheek1313 <matthewdavis@MacBook-Pro.local>
2017-01-26 21:02:30 -0700

    Starting Over Without Plugins

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 
-$(function($) {
+
+function selectPages(){
     $('ul li').addClass('paginate');
     let pages = $('.paginate');
     let numPages = pages.length;
     let itemsPerPage = 10;
-)};
+    pages.slice(itemsPerPage).hide();
+}
+selectPages();
